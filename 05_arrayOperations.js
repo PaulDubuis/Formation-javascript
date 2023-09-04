22368558ab547f1565db2433d849ed2b8472d5bb
Paul Dubuis <paul.dubuis@citegestion.com>
2023-09-04 11:48:08 +0200

    Remove print test functions

--- 05_arrayOperations.js
+++ 05_arrayOperations.js
@@ -57,17 +57,4 @@ const sum = (array) => ( array.reduce((res, i) => res + i, 0 ))
 
 const findUserById = (array, id) => ( array.find((user) => user.id === id).name)
 
-
-const PrintTest = (a, b, c) => {
-  console.log(!c ? a(b) : a(b, c))
-}
-const a1 = [1, 2, 3, 4, 5]
-const a2 = ["A1", "A2", "A3", "DE", "TR"]
-const a3 = [1, 2, 3]
-const a4 = [ {id: 1, name: 'John'}, {id: 2, name: 'Doe'}, {id: 3, name: 'Foo'}, {id: 4, name: 'Bar'} ]
-PrintTest(multiplyByTwo, a1)
-PrintTest(filterNameStartByA, a2)
-PrintTest(sum, a3)
-PrintTest(findUserById, a4, 2)
-
 module.exports = {multiplyByTwo, filterNameStartByA, sum, findUserById};
